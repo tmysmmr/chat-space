0eba35b6f6cdb3be61cdef59039ea2bfac380d27
tmysmmr <motha.815@gmail.com>
2019-09-19 20:13:54 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function() {
     //functionによって関数の宣言 buildHTMlは関数の名前 messageは引数
     //function 関数名 (引数){関数を定義する文}
     var image = message.image.url ? `<img class="message__image" src=${message.image.url}>` : "";
-    var html = `<div class="message">
+    var html = `<div class="message" data-id="${message.id}">
                   <div class="message__upper-info">
                     <p class="message__upper-info__talker">
                       ${message.user_name}
@@ -15,10 +15,11 @@ $(function() {
                   <p class="message__text">
                     ${message.content}
                   </p>
-                    ${message.image}
+                    ${image}
                 </div>`
     return html;
   }
+
   $('#new_message').on('submit', function(e){
     //onメソッドは第一引数にイベント名、第2引数にそのイベントが実行された時に実行されるfunctionを指定できる
     e.preventDefault();
@@ -29,7 +30,6 @@ $(function() {
       type: "POST",
       data: formData,
       dataType: 'json',
-      //応答データをJson形式にするということ
       processData: false,
       contentType: false
     })
@@ -37,7 +37,7 @@ $(function() {
       var html = buildHTML(message);
       //htmlという変数にcreate.json.jbuilderのデータを代入する
       $('.messages').append(html);
-      //appendメソッドとは？→$(‘セレクタ’).append(‘追加するもの’);
+      //appendメソッドとは？→$('セレクタ').append('追加するもの');
       $('.submit-btn').prop('disabled', false);
       //propメソッドとは？→$('セレクタ').prop('プロパティ名');
       //disabled属性をfalseにするとこによってSendボタンが2回目以降も有効になる
@@ -45,8 +45,32 @@ $(function() {
       $('#new_message')[0].reset();
     })
     .fail(function(){
-      //サーバーエラー(通信に失敗した時)fail関数が呼ばれる
       alert('error');
     })
   })
+
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message').last().data('id');
+      $.ajax({
+        url: "api/messages",
+        type: "GET",
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+
+      .done(function(messages) {
+        var insertHTML = '';
+        messages.forEach(function(message) {
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+        });
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
+      })
+      .fail(function() {
+        console.log('error');
+      })
+    }
+  }
+  setInterval(reloadMessages, 5000);
 });
